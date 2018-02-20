231c1a905d59e3151fff2fb05a0fc1d4e90ca1e6
MD Showman <mdshowman@gmail.com>
2018-02-20 11:25:16 -0500

    Correcting Eclipse project errors while using GitHub

---
+++ src/Roster.java
@@ -0,0 +1,21 @@
+import java.util.Vector;
+
+public class Roster {
+	
+	private Vector<Student> studentList = new Vector<Student>();
+	
+	public Roster(Student[] list) {
+		for (Student s : list) {
+			this.studentList.add(s);
+		}
+	}
+	
+	public void AddStudent(Student s) {
+		this.studentList.addElement(s);
+	}
+	
+	public void RemoveStudent(Student s) {
+		this.studentList.remove(s);
+	}
+
+}
