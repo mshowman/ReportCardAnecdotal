e1a54ede7ea5c1d791a0377c798e7d6519dbacda
MD Showman <mdshowman@gmail.com>
2018-02-20 14:54:27 -0500

    Added GUI bits. Nothing that will be kept, just playing with GUI.

--- src/RCA_main.java
+++ src/RCA_main.java
@@ -1,9 +1,35 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
+/*
+ * RCA_main class
+ * 
+ * GUI for application
+ * 
+ * Code as of 2-20 is just me playing around with GUI, nothing planned to be kept just yet
+ */
 public class RCA_main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Roster studentList = new Roster();
+		
+		// create frame and button
+		JFrame mainFrame = new JFrame();
+		JButton addStudent = new JButton();
+		
+		// add text to button
+		// set where button is on frame
+		addStudent.setText("Add a Student");
+		addStudent.setBounds(290, 350, 100, 20);
+		
+		// add button to frome
+		// set size of frame
+		mainFrame.add(addStudent);
+		mainFrame.setSize(400, 400);
+		
+		// show frame
+		mainFrame.setLayout(null);
+		mainFrame.setVisible(true);
 	}
+	
 
 }
