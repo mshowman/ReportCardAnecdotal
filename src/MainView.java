2a2ab7b5dbb96107c8f11c91a1d09b23996b09f8
MD Showman <mdshowman@gmail.com>
2018-02-24 19:52:28 -0500

    Add, Edit, and Remove students are now in the same view. Menubar now has one menu (File) with four menu items (Roster, Edit Subject Anecdotal, Run Report, and Quit). Will replace menubar with Tabbed view since having one menu seems pointless.

--- src/MainView.java
+++ src/MainView.java
@@ -33,39 +33,31 @@ public class MainView extends JFrame {
 		// menubar
 		JMenuBar navBar = new JMenuBar();
 
-		// roster menu
-		JMenu rosterMenu = new JMenu("Roster");
+		// file menu
+		JMenu fileMenu = new JMenu("File");
 
-		// add a student option of roster menu
-		JMenuItem addRoster = new JMenuItem("Add a student");
-		rosterMenu.add(addRoster);
+		// roster menu item
+		JMenuItem rosterMenu = new JMenuItem("Roster");
+		RosterMenuListener objRosterMenuListener = new RosterMenuListener();
+		rosterMenu.addActionListener(objRosterMenuListener);
+		fileMenu.add(rosterMenu);
 
-		// edit a student option of roster menu
-		JMenuItem editRoster = new JMenuItem("Edit a student");
-		rosterMenu.add(editRoster);
+		// subject menu item
+		JMenuItem subjectMenu = new JMenuItem("Edit Anecdotal for Subject");
+		fileMenu.add(subjectMenu);
 
-		// remove a student option of roster menu
-		JMenuItem removeRoster = new JMenuItem("Remove a student");
-		rosterMenu.add(removeRoster);
+		// report menu item
+		JMenuItem reportMenu = new JMenuItem("Run Report");
+		fileMenu.add(reportMenu);
 
-		// action listener for add a student option
-		AddStudentListener objAddListener = new AddStudentListener();
-		addRoster.addActionListener(objAddListener);
+		// menu separator
+		fileMenu.addSeparator();
 
-		// action listener for edit a student option
-		EditStudentListener objEditListener = new EditStudentListener();
-		editRoster.addActionListener(objEditListener);
+		// quit menu item
+		JMenuItem quitMenu = new JMenuItem("Quit");
+		fileMenu.add(quitMenu);
 
-		// subject menu
-		JMenu subjectMenu = new JMenu("Edit Anecdotal for Subject");
-
-		// report menu
-		JMenu reportMenu = new JMenu("Run Report");
-
-		// add menus to menubar
-		navBar.add(rosterMenu);
-		navBar.add(subjectMenu);
-		navBar.add(reportMenu);
+		navBar.add(fileMenu);
 
 		// sets menubar of frame to navbar
 		setJMenuBar(navBar);
@@ -79,199 +71,21 @@ public class MainView extends JFrame {
 		setVisible(true);
 	}
 
-	// sets view to default view
+	// sets view to default view (blank)
 	public void defaultView() {
 		c.removeAll();
 		c.validate();
 		c.repaint();
 	}
 
-	// sets view to add student form
-	public void addStudentView() {
-		// clear container
-		c.removeAll();
-		
-		// creates panel to hold component panels
-		JPanel viewPanel = new JPanel(new GridLayout(7, 2));
-		viewPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
-
-		// creates panel to hold label and textfield for name
-		JPanel namePanel = new JPanel(new GridLayout(1, 2));
-		namePanel.setBorder(new EmptyBorder(20, 20, 20, 20));
-
-		// creates label and textfield for name
-		JLabel nameLabel = new JLabel("Student Name:");
-		JTextField nameField = new JTextField();
-
-		// add items to panel
-		namePanel.add(nameLabel);
-		namePanel.add(nameField);
-
-		// add panel to container
-		viewPanel.add(namePanel);
-
-		// creates panel to hold label and radio buttons for general grade
-		JPanel generalPanel = new JPanel(new GridLayout(1, 4));
-		generalPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
-
-		// creates label and radio buttons
-		JLabel generalLabel = new JLabel("General Grade:");
-		JRadioButton genPRadio = new JRadioButton("P");
-		JRadioButton genMRadio = new JRadioButton("M");
-		JRadioButton genERadio = new JRadioButton("E");
-
-		// adds radio buttons to group to limit selection to one option
-		ButtonGroup generalButtons = new ButtonGroup();
-		generalButtons.add(genPRadio);
-		generalButtons.add(genMRadio);
-		generalButtons.add(genERadio);
-
-		// add items to panel
-		generalPanel.add(generalLabel);
-		generalPanel.add(genPRadio);
-		generalPanel.add(genMRadio);
-		generalPanel.add(genERadio);
-
-		// add panel to container
-		viewPanel.add(generalPanel);
-
-		// creates panel to hold label and radio buttons for math grade
-		JPanel mathPanel = new JPanel(new GridLayout(1, 4));
-		mathPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
-
-		// creates label and radio buttons
-		JLabel mathLabel = new JLabel("Math Grade:");
-		JRadioButton mathPRadio = new JRadioButton("P");
-		JRadioButton mathMRadio = new JRadioButton("M");
-		JRadioButton mathERadio = new JRadioButton("E");
-
-		// adds radio buttons to group to limit selection to one option
-		ButtonGroup mathButtons = new ButtonGroup();
-		mathButtons.add(mathPRadio);
-		mathButtons.add(mathMRadio);
-		mathButtons.add(mathERadio);
-
-		// add items to panel
-		mathPanel.add(mathLabel);
-		mathPanel.add(mathPRadio);
-		mathPanel.add(mathMRadio);
-		mathPanel.add(mathERadio);
-
-		// add panel to container
-		viewPanel.add(mathPanel);
-
-		// creates panel to hold label and radio buttons for ELA grade
-		JPanel elaPanel = new JPanel(new GridLayout(1, 4));
-		elaPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
-
-		// creates label and radio buttons
-		JLabel elaLabel = new JLabel("ELA Grade:");
-		JRadioButton elaPRadio = new JRadioButton("P");
-		JRadioButton elaMRadio = new JRadioButton("M");
-		JRadioButton elaERadio = new JRadioButton("E");
-
-		// adds radio buttons to group to limit selection to one option
-		ButtonGroup elaButtons = new ButtonGroup();
-		elaButtons.add(elaPRadio);
-		elaButtons.add(elaMRadio);
-		elaButtons.add(elaERadio);
-
-		// add items to panel
-		elaPanel.add(elaLabel);
-		elaPanel.add(elaPRadio);
-		elaPanel.add(elaMRadio);
-		elaPanel.add(elaERadio);
-
-		// add panel to container
-		viewPanel.add(elaPanel);
-
-		// creates panel to hold label and radio buttons for science grade
-		JPanel sciencePanel = new JPanel(new GridLayout(1, 4));
-		sciencePanel.setBorder(new EmptyBorder(20, 20, 20, 20));
-
-		// creates label and radio buttons
-		JLabel scienceLabel = new JLabel("Science Grade:");
-		JRadioButton sciencePRadio = new JRadioButton("P");
-		JRadioButton scienceMRadio = new JRadioButton("M");
-		JRadioButton scienceERadio = new JRadioButton("E");
-
-		// adds radio buttons to group to limit selection to one option
-		ButtonGroup scienceButtons = new ButtonGroup();
-		scienceButtons.add(sciencePRadio);
-		scienceButtons.add(scienceMRadio);
-		scienceButtons.add(scienceERadio);
-
-		// add items to panel
-		sciencePanel.add(scienceLabel);
-		sciencePanel.add(sciencePRadio);
-		sciencePanel.add(scienceMRadio);
-		sciencePanel.add(scienceERadio);
-
-		// add panel to container
-		viewPanel.add(sciencePanel);
-
-		// creates panel to hold label and radio buttons for social studies grade
-		JPanel ssPanel = new JPanel(new GridLayout(1, 4));
-		ssPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
-
-		// creates label and radio buttons
-		JLabel ssLabel = new JLabel("Social Studies Grade:");
-		JRadioButton ssPRadio = new JRadioButton("P");
-		JRadioButton ssMRadio = new JRadioButton("M");
-		JRadioButton ssERadio = new JRadioButton("E");
-
-		// adds radio buttons to group to limit selection to one option
-		ButtonGroup ssButtons = new ButtonGroup();
-		ssButtons.add(ssPRadio);
-		ssButtons.add(ssMRadio);
-		ssButtons.add(ssERadio);
-
-		// add items to panel
-		ssPanel.add(ssLabel);
-		ssPanel.add(ssPRadio);
-		ssPanel.add(ssMRadio);
-		ssPanel.add(ssERadio);
-
-		// add panel to container
-		viewPanel.add(ssPanel);
-
-		// creates panel for buttons
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
-
-		// creates buttons for save and cancel
-		JButton saveButton = new JButton("Save Student");
-
-		JButton cancelButton = new JButton("Cancel");
-		CancelListener objCancelListener = new CancelListener();
-		cancelButton.addActionListener(objCancelListener);
-
-		// add buttons to panel
-		buttonPanel.add(saveButton);
-		buttonPanel.add(cancelButton);
-
-		// add panel to container
-		viewPanel.add(buttonPanel);
-		
-		// add view panel to container
-		c.add(viewPanel);
-
-		// activates name textfield
-		nameField.grabFocus();
-
-		// redraws container contents
-		c.validate();
-		c.repaint();
-	}
-
-	// sets view to edit student form
-	public void editStudentView() {
+	// sets view to Roster
+	public void showRosterView() {
 		// clear container
 		c.removeAll();
 
 		// creates jlist object to show roster contents
 		// adds dummy items to list for display testing
-		DefaultListModel<String> studentList = new DefaultListModel();
+		DefaultListModel<String> studentList = new DefaultListModel<String>();
 		studentList.addElement("Matt Showman");
 		studentList.addElement("Nate Lewis");
 		studentList.addElement("Matt Showman");
@@ -310,31 +124,42 @@ public class MainView extends JFrame {
 		studentList.addElement("Nate Lewis");
 		studentList.addElement("Matt Showman");
 		studentList.addElement("Nate Lewis");
-		
+
 		// creates jlist to store roster list
 		// limits selection to one student at a time
 		JList<String> rosterList = new JList<String>(studentList);
 		rosterList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		rosterList.setLayoutOrientation(JList.VERTICAL);
-		
+
 		// creates scrollbar for jlist object
 		JScrollPane rosterScroller = new JScrollPane(rosterList);
-		rosterScroller.setPreferredSize(new Dimension(10, 600));
+		rosterScroller.setPreferredSize(new Dimension(200, 475));
 		rosterScroller.setAlignmentX(RIGHT_ALIGNMENT);
 		rosterScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		rosterScroller.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		rosterScroller.getVerticalScrollBar().setUnitIncrement(10);
-		
+
 		// creates panel to hold roster list
-		JPanel rosterPanel = new JPanel(new GridLayout(1, 1));
+		JPanel rosterPanel = new JPanel();
 		rosterPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
 		rosterPanel.setPreferredSize(new Dimension(200, 600));
 
-		// adds scrollbar (and attached jlist) to panel
-		rosterPanel.add(rosterScroller);
-		
+		// creates add and delete buttons
+		JButton addStudent = new JButton("+");
+		JButton removeStudent = new JButton("-");
+
+		// creates panel to hold add and remove buttons
+		JPanel rosterButtons = new JPanel();
+		rosterButtons.add(addStudent);
+		rosterButtons.add(removeStudent);
+
+		// adds buttons, scrollbar (and attached jlist) to panel
+		rosterPanel.add(rosterScroller, BorderLayout.CENTER);
+		rosterPanel.add(rosterButtons, BorderLayout.PAGE_END);
+
 		// creates panel to hold form fields
 		JPanel formPanel = new JPanel(new GridLayout(7, 2));
-		
+
 		// creates panel to hold label and textfield for name
 		JPanel namePanel = new JPanel(new GridLayout(1, 2));
 		namePanel.setBorder(new EmptyBorder(20, 20, 20, 20));
@@ -477,7 +302,7 @@ public class MainView extends JFrame {
 
 		// creates panel for buttons
 		JPanel buttonPanel = new JPanel();
-		buttonPanel.setBorder(new EmptyBorder(20,20,20,20));
+		buttonPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
 
 		// creates buttons for save and cancel
 		JButton saveButton = new JButton("Save Student");
@@ -496,7 +321,7 @@ public class MainView extends JFrame {
 		// add rosterPanel and formPanel to layout panel
 		c.add(rosterPanel, BorderLayout.LINE_START);
 		c.add(formPanel, BorderLayout.CENTER);
-		
+
 		// redraws screen
 		c.validate();
 		c.repaint();
@@ -506,19 +331,11 @@ public class MainView extends JFrame {
 
 	}
 
-	private class AddStudentListener implements ActionListener {
+	private class RosterMenuListener implements ActionListener {
 
 		public void actionPerformed(ActionEvent arg0) {
-			addStudentView();
+			showRosterView();
 		}
-	}
-
-	private class EditStudentListener implements ActionListener {
-
-		public void actionPerformed(ActionEvent arg0) {
-			editStudentView();
-		}
-
 	}
 
 	private class SubjectMenuListener implements ActionListener {
